0c04dbace69ff26ab344e6c9d8031ce1785624ae
Hakin Gutierrez <hakingutierrez99@gmail.com>
2023-05-22 03:46:08 -0600

    ProyectoFinalJS

---
+++ src/Data.js
@@ -0,0 +1,50 @@
+let shopItemsData =[
+{
+    id: "CSTY",
+    name : "Cámara 1488HD Stryker para endoscopia",
+    price: 150000,
+    img: "img/endo.png",
+    desc: "Endoscopia",
+    cantidad: 10
+},
+{
+    id: "CIMTR",
+    name : "Cámara IM8000 TrueHD Conmed",
+    price: 100000,
+    img: "img/endo1.png",
+    desc: "Endoscopia",
+    cantidad: 4
+},
+{
+    id: "MSVR",
+    name : "Monitor signos vitales mod UMEC10 mindray",
+    price: 20000,
+    img: "img/MSV.png",
+    desc: "Cardiología Monitores",
+    cantidad: 5
+},
+{
+    id: "MLEDSONY",
+    name : "Monitor LED 4K Sony Medical",
+    price: 15000,
+    img: "img/mtr.png",
+    desc: "Monitores",
+    cantidad: 3
+},
+{
+    id: "SONOECG",
+    name : "Electrocardiógrafo SONOECG12000 12 canales",
+    price: 32000,
+    img: "img/cardio.webp",
+    desc: "Cardiología",
+    cantidad: 10
+},
+{
+    id: "SONODES",
+    name : "Desfibrilador Sono-AED-Automatico",
+    price: 49000,
+    img: "img/des.webp",
+    desc: "Cardiología",
+    cantidad: 6
+}
+]
